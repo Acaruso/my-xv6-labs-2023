f2d6fbdb3f72abdf8523bda107a20508bb3696af
Anthony Caruso <anthonycaruso102@gmail.com>
2024-06-22 18:14:51 +0000

    start find

---
+++ user/find.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+const int STDIN = 0;
+const int STDOUT = 1;
+
+// find all files in a directory tree with some name
+// syntax:
+//   find <starting-directory> <file-to-find>
+
+int main() {
+}
