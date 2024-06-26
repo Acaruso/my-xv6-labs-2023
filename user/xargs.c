bf798006732a2108e875d56ca3c1bb885e07b367
Anthony Caruso <anthonycaruso102@gmail.com>
2024-06-26 12:46:22 +0000

    finish xargs

--- user/xargs.c
+++ user/xargs.c
@@ -4,39 +4,15 @@
 #include "kernel/fcntl.h"
 #include "kernel/param.h"
 
-// int main(int argc, char *argv[]) {
-//     char* command = argv[1];
-//     char* new_argv[MAXARG];
-
-//     int new_argc = argc - 1;
-//     int old_idx = 2;
-//     int new_idx = 0;
-//     while (old_idx < argc) {
-//         new_argv[new_idx++] = argv[old_idx++];
-//     }
-//     new_argv[new_idx] = 0;  // argv is null-terminated
-
-//     if (fork() == 0) {
-//         exec(command, new_argv);
-//     } else {
-//         // do stuff
-//     }
-// }
-
 int STDIN = 0;
-int STDOUT = 1;
 
-void make_new_argv(int argc, char *argv[], char* new_argv[]);
 void run_command(char* command, char* argv[]);
-int is_space(char c);
-int is_newline(char c);
+void make_new_argv(int argc, char *argv[], char* new_argv[]);
 void push_to_argv(char* new_item, char* argv[]);
-void pop_from_argv(char* argv[]);
 
 int main(int argc, char *argv[]) {
     char* command = argv[1];
     char* new_argv[MAXARG];
-
     make_new_argv(argc, argv, new_argv);
 
     if (fork() == 0) {
@@ -56,7 +32,7 @@ void run_command(char* command, char* argv[]) {
     while (bytes_read != 0) {
         int start_idx = 0;
         for (int i = 0; i < bytes_read; i++) {
-            if (is_newline(buf[i])) {
+            if (buf[i] == '\n') {
                 buf[i] = '\0';
 
                 if (fork() == 0) {
@@ -83,29 +59,6 @@ void make_new_argv(int argc, char *argv[], char* new_argv[]) {
     new_argv[new_idx] = 0;  // argv is null-terminated
 }
 
-int is_newline(char c) {
-    if (c == '\n') {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
-int is_space(char c) {
-    if (
-        c == ' '
-        || c == '\t'
-        || c == '\n'
-        || c == '\f'
-        || c == '\r'
-        || c == '\v'
-    ) {
-        return 1;
-    } else {
-        return 0;
-    }
-}
-
 void push_to_argv(char* new_item, char* argv[]) {
     int i = 0;
     while (argv[i] != 0) {
@@ -113,14 +66,4 @@ void push_to_argv(char* new_item, char* argv[]) {
     }
     argv[i++] = new_item;
     argv[i] = 0;
-}
-
-void pop_from_argv(char* argv[]) {
-    int i = 0;
-    while (argv[i] != 0) {
-        i++;
-    }
-    if (i - 1 >= 0) {
-        argv[i - 1] = 0;
-    }
 }
