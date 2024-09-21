b9b89960c126ebba015b988b9ae68035db498c8f
Anthony Caruso <anthonycaruso102@gmail.com>
2024-09-21 11:15:04 -0400

    memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,6 +10,7 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+struct kmem* find_free_memory(int cpu_id);
 
 extern char end[];  // first address after kernel.
                     // defined by kernel.ld.
@@ -18,13 +19,17 @@ struct run {
     struct run *next;
 };
 
-struct {
+struct kmem {
     struct spinlock lock;
     struct run *freelist;
-} kmem;
+};
+
+struct kmem kmem_arr[NCPU];
 
 void kinit() {
-    initlock(&kmem.lock, "kmem");
+    for (int i = 0; i < NCPU; i++) {
+        initlock(&kmem_arr[i].lock, "kmem");
+    }
     freerange(end, (void *)PHYSTOP);
 }
 
@@ -39,19 +44,24 @@ void freerange(void *pa_start, void *pa_end) {
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
 void kfree(void *pa) {
-    struct run *r;
-
-    if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP) panic("kfree");
+    if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP) {
+        panic("kfree");
+    }
 
     // Fill with junk to catch dangling refs.
     memset(pa, 1, PGSIZE);
 
-    r = (struct run *)pa;
+    struct run *r = (struct run *)pa;
 
-    acquire(&kmem.lock);
-    r->next = kmem.freelist;
-    kmem.freelist = r;
-    release(&kmem.lock);
+    push_off();
+    int cpu_id = cpuid();
+    pop_off();
+    struct kmem *kmem = &kmem_arr[cpu_id];
+
+    acquire(&kmem->lock);
+    r->next = kmem->freelist;
+    kmem->freelist = r;
+    release(&kmem->lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -60,11 +70,51 @@ void kfree(void *pa) {
 void *kalloc(void) {
     struct run *r;
 
-    acquire(&kmem.lock);
-    r = kmem.freelist;
-    if (r) kmem.freelist = r->next;
-    release(&kmem.lock);
+    push_off();
+    int cpu_id = cpuid();
+    pop_off();
+    struct kmem *kmem = &kmem_arr[cpu_id];
 
-    if (r) memset((char *)r, 5, PGSIZE);  // fill with junk
-    return (void *)r;
+    acquire(&kmem->lock);
+
+    if (kmem->freelist == 0) {
+        release(&kmem->lock);
+        kmem = find_free_memory(cpu_id);
+        if (kmem == 0) {
+            // system is out of memory
+            return 0;
+        }
+        // else if kmem != 0, then free memory was found
+        // kmem is returned with lock held
+        r = kmem->freelist;
+        kmem->freelist = r->next;
+        release(&kmem->lock);
+        memset((char *)r, 5, PGSIZE);
+        return (void *)r;
+    } else {
+        r = kmem->freelist;
+        kmem->freelist = r->next;
+        release(&kmem->lock);
+        memset((char *)r, 5, PGSIZE);
+        return (void *)r;
+    }
+}
+
+struct kmem* find_free_memory(int cpu_id) {
+    struct kmem *kmem = 0;
+
+    for (int i = 0; i < NCPU; i++) {
+        if (i == cpu_id) {
+            continue;
+        }
+        kmem = &kmem_arr[i];
+
+        acquire(&kmem->lock);
+        if (kmem->freelist != 0) {
+            return kmem;
+        }
+        release(&kmem->lock);
+    }
+
+    return 0;
 }
